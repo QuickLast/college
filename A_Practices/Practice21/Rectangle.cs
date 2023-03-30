626f03a24cabad9ea11cbee0dcc5d017e5d5fc57
QuickLast <99359492+QuickLast@users.noreply.github.com>
2023-03-30 16:41:33 +0300

    Add files via upload
    Use git to import this code into your project

---
+++ A_Practices/Practice21/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice21
+{
+    internal class Rectangle : GeometricalFigure
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            TypeOfFigure = "Rectangle";
+            Width = width;
+            Height = height;
+        }
+        public override double squareOfFigure()
+        {
+            return Height * Width;
+        }
+        public override string ToString()
+        {
+            return $"Width of the {TypeOfFigure} = {Width},\nHeight of the {TypeOfFigure} = {Height}, \nSquare of the {TypeOfFigure} = {squareOfFigure()}\n";
+        }
+    }
+}
