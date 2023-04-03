1d209ed3cc1df42d35ee81d86e99d7c770230b05
QuickLast <99359492+QuickLast@users.noreply.github.com>
2023-04-03 10:11:57 +0300

    Add files via upload

---
+++ A_Practices/Practice21/Practice21/GeometricalFigure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice21
+{
+    internal abstract class GeometricalFigure
+    {
+        public string TypeOfFigure { get; set; }
+        public virtual double squareOfFigure()
+        {
+            return -1.0;
+        }
+       
+    }
+}
