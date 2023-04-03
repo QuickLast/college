eb99fa68ef6ed69c661f6a71164efd6c2fc83373
QuickLast <99359492+QuickLast@users.noreply.github.com>
2023-04-03 10:10:16 +0300

    Add files via upload

---
+++ A_Practices/Practice22/Practice22/ComplexClass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice22
+{
+    internal class ComplexClass
+    {
+        public string Imaginary { get; set; }
+        public string Real { get; set; }
+        public static ComplexClass operator +(ComplexClass complex1, ComplexClass complex2)
+        {
+            int imaginaryFirst = Convert.ToInt32(complex1.Imaginary.Substring(0, complex1.Imaginary.Length - 1));
+            int imaginarySecond = Convert.ToInt32(complex2.Imaginary.Substring(0, complex2.Imaginary.Length - 1));
+
+            int realFirst = Convert.ToInt32(complex1.Real);
+            int realSecond = Convert.ToInt32(complex2.Real);
+
+            ComplexClass sum = new ComplexClass { Imaginary = Convert.ToString(imaginaryFirst + imaginarySecond) + "i", Real = Convert.ToString(realFirst + realSecond) };
+
+            if (sum.Imaginary == "1i") sum.Imaginary = "i";
+            else if (sum.Imaginary == "-1i") sum.Imaginary = "-i";
+
+            return sum;
+        }
+        public static ComplexClass operator -(ComplexClass complex1, ComplexClass complex2)
+        {
+            int imaginaryFirst = Convert.ToInt32(complex1.Imaginary.Substring(0, complex1.Imaginary.Length - 1));
+            int imaginarySecond = Convert.ToInt32(complex2.Imaginary.Substring(0, complex2.Imaginary.Length - 1));
+
+            int realFirst = Convert.ToInt32(complex1.Real);
+            int realSecond = Convert.ToInt32(complex2.Real);
+
+
+            ComplexClass difference = new ComplexClass { Imaginary = Convert.ToString(imaginaryFirst - imaginarySecond) + "i", Real = Convert.ToString(realFirst - realSecond) };
+
+            if (difference.Imaginary == "1i") difference.Imaginary = "i";
+            else if (difference.Imaginary == "-1i") difference.Imaginary = "-i";
+
+            return difference;
+        }
+        public static ComplexClass operator *(ComplexClass complex1, ComplexClass complex2)
+        {
+            int imaginaryFirst = Convert.ToInt32(complex1.Imaginary.Substring(0, complex1.Imaginary.Length - 1));
+            int imaginarySecond = Convert.ToInt32(complex2.Imaginary.Substring(0, complex2.Imaginary.Length - 1));
+
+            int realFirst = Convert.ToInt32(complex1.Real);
+            int realSecond = Convert.ToInt32(complex2.Real);
+
+
+            ComplexClass multiplex = new ComplexClass { Imaginary = Convert.ToString(imaginaryFirst*realSecond+realFirst*imaginarySecond) + "i", Real = Convert.ToString(realFirst*realSecond-imaginaryFirst*imaginarySecond) };
+
+            if (multiplex.Imaginary == "1i") multiplex.Imaginary = "i";
+            else if (multiplex.Imaginary == "-1i") multiplex.Imaginary = "-i";
+
+            return multiplex;
+        }
+
+        public static ComplexClass operator /(ComplexClass complex1, ComplexClass complex2)
+        {
+            double imaginaryFirst = Convert.ToDouble(complex1.Imaginary.Substring(0, complex1.Imaginary.Length - 1));
+            double imaginarySecond = Convert.ToDouble(complex2.Imaginary.Substring(0, complex2.Imaginary.Length - 1));
+
+            double realFirst = Convert.ToDouble(complex1.Real);
+            double realSecond = Convert.ToDouble(complex2.Real);
+
+
+            ComplexClass division = new ComplexClass { Imaginary = Convert.ToString((imaginaryFirst*realSecond-realFirst*imaginarySecond)/(realSecond*realSecond + imaginarySecond*imaginarySecond)) + "i", Real = Convert.ToString((realFirst*realSecond+imaginaryFirst*imaginarySecond)/(realSecond * realSecond + imaginaryFirst * imaginaryFirst)) };
+
+            if (division.Imaginary == "1i") division.Imaginary = "i";
+            else if (division.Imaginary == "-1i") division.Imaginary = "-i";
+
+            return division;
+        }
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}";
+        }
+    }
+}
---
+++ A_Practices/Practice22/Practice22/Program.cs
@@ -0,0 +1,28 @@
+﻿using Practice22;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+ComplexClass complex1 = new ComplexClass { Imaginary = "2i", Real = "2" };
+ComplexClass complex2 = new ComplexClass { Imaginary = "-3i", Real = "-3" };
+
+Console.WriteLine($"Даны 2 комплексных числа: {complex1}; {complex2}");
+
+/* Сумма */
+ComplexClass sum = complex1 + complex2;
+Console.WriteLine("Сумма = " + sum);
+
+/* Вычитание */
+ComplexClass difference = complex1 - complex2;
+Console.WriteLine("Разность = " + difference);
+
+/* Умножение */
+ComplexClass multiplication = complex1 * complex2;
+Console.WriteLine("Умножение = " + multiplication);
+
+/* Деление */
+ComplexClass division = complex1 / complex2;
+Console.WriteLine("Деление = " + division);
